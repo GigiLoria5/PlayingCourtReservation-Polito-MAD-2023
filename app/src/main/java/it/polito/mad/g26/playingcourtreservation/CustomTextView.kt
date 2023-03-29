688bc77f1348d782ddd52a8e8760dc7b634c9d16
Luigi Loria <41837640+GigiLoria5@users.noreply.github.com>
2023-03-29 10:05:14 +0200

    feat: add profile secondary information

---
+++ app/src/main/java/it/polito/mad/g26/playingcourtreservation/CustomTextView.kt
@@ -0,0 +1,29 @@
+package it.polito.mad.g26.playingcourtreservation
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+import android.widget.TextView
+
+class CustomTextView(
+    context: Context,
+    attrs: AttributeSet
+) : LinearLayout(context, attrs) {
+
+    init {
+        inflate(context, R.layout.custom_text_view, this)
+
+        val customAttributesStyle =
+            context.obtainStyledAttributes(attrs, R.styleable.CustomTextView, 0, 0)
+
+        val title = findViewById<TextView>(R.id.title)
+        val value = findViewById<TextView>(R.id.value)
+
+        try {
+            title.text = customAttributesStyle.getString(R.styleable.CustomTextView_title)
+            value.text = customAttributesStyle.getString(R.styleable.CustomTextView_value)
+        } finally {
+            customAttributesStyle.recycle()
+        }
+    }
+}
