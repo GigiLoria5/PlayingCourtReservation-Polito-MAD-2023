c535b44e356435dd8f5028b070ef65e64de0fb59
Luigi Loria <41837640+GigiLoria5@users.noreply.github.com>
2023-03-29 17:40:32 +0200

    feat: add user reliability icons

--- app/src/main/java/it/polito/mad/g26/playingcourtreservation/ShowProfileActivity.kt
+++ app/src/main/java/it/polito/mad/g26/playingcourtreservation/ShowProfileActivity.kt
@@ -2,10 +2,18 @@ package it.polito.mad.g26.playingcourtreservation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.widget.TooltipCompat
 
 class ShowProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
+
+        // Add Tooltips
+        val warningIcon = findViewById<ImageView>(R.id.warning_icon)
+        TooltipCompat.setTooltipText(warningIcon, getString(R.string.warning_icon_tooltip))
+        val dangerIcon = findViewById<ImageView>(R.id.danger_icon)
+        TooltipCompat.setTooltipText(dangerIcon, getString(R.string.danger_icon_tooltip))
     }
 }
