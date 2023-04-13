eb4f367f6c66b3221128f4819fcb49e9c8d59031
Luigi Loria <41837640+GigiLoria5@users.noreply.github.com>
2023-04-13 12:21:05 +0200

    Revert "Feat/photo"

---
+++ app/src/main/java/it/polito/mad/g26/playingcourtreservation/ShowProfileActivity.kt
@@ -0,0 +1,11 @@
+package it.polito.mad.g26.playingcourtreservation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_profile)
+    }
+}
