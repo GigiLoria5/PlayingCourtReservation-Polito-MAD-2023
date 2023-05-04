0bd2dc21367895cd3a09ebb875a53c1cc8c5b949
RiccardoStud <s292520@studenti.polito.it>
2023-05-04 20:58:56 +0200

    feat: Spinner implemented
    Spinner implemented

--- app/src/main/java/it/polito/mad/g26/playingcourtreservation/fragment/ReservationXFragment.kt
+++ app/src/main/java/it/polito/mad/g26/playingcourtreservation/fragment/ReservationXFragment.kt
@@ -2,18 +2,27 @@ package it.polito.mad.g26.playingcourtreservation.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import it.polito.mad.g26.playingcourtreservation.R
 
 
 
 class ReservationXFragment : Fragment(R.layout.fragment_reservation_x) {
 
+    private lateinit var spinnerSer: Spinner
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val banana = "Banana"
+
+        spinnerSer = view.findViewById(R.id.spinner)
+
+        val adapterSer = ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.services_array,
+            android.R.layout.simple_spinner_item
+        )
+        spinnerSer.adapter = adapterSer
     }
 
 }
