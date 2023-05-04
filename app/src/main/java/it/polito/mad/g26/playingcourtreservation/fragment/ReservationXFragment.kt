dc6c9ac6a29d5586bb3ea452e609c91912e725a3
RiccardoStud <s292520@studenti.polito.it>
2023-05-04 20:45:26 +0200

    feat: ReservationX fragment+layout creation
    Creation of fragment ReservationX

---
+++ app/src/main/java/it/polito/mad/g26/playingcourtreservation/fragment/ReservationXFragment.kt
@@ -0,0 +1,19 @@
+package it.polito.mad.g26.playingcourtreservation.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import it.polito.mad.g26.playingcourtreservation.R
+
+
+
+class ReservationXFragment : Fragment(R.layout.fragment_reservation_x) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val banana = "Banana"
+    }
+
+}
