97aa8d42142963f24fd45960b4de69617ac5c1fd
RiccardoStud <s292520@studenti.polito.it>
2023-05-04 21:21:01 +0200

    feat: reservationX 2nd layout
    2nd layout + new type of spinner

--- app/src/main/java/it/polito/mad/g26/playingcourtreservation/fragment/ReservationXFragment.kt
+++ app/src/main/java/it/polito/mad/g26/playingcourtreservation/fragment/ReservationXFragment.kt
@@ -3,26 +3,36 @@ package it.polito.mad.g26.playingcourtreservation.fragment
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import it.polito.mad.g26.playingcourtreservation.R
 
 
 
-class ReservationXFragment : Fragment(R.layout.fragment_reservation_x) {
+class ReservationXFragment : Fragment(R.layout.fragment_reservation_x){
 
     private lateinit var spinnerSer: Spinner
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        //Set spinner and adapter
         spinnerSer = view.findViewById(R.id.spinner)
+        val adapterSer= ArrayAdapter.createFromResource(requireContext(), R.array.services_array , android.R.layout.simple_spinner_item)
+        spinnerSer.adapter=adapterSer
 
-        val adapterSer = ArrayAdapter.createFromResource(
-            requireContext(),
-            R.array.services_array,
-            android.R.layout.simple_spinner_item
-        )
-        spinnerSer.adapter = adapterSer
+        spinnerSer.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                spinnerSer.setSelection(0)
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Do nothing
+            }
+
+        }
+
     }
 
 }
