b70b8fd2f6c3c0349bc59703bd9a7efdfcad134b
Luigi Loria <41837640+GigiLoria5@users.noreply.github.com>
2023-05-05 10:55:49 +0200

    feat: show/hide bottom navigation when needed

--- app/src/main/java/it/polito/mad/g26/playingcourtreservation/activity/MainActivity.kt
+++ app/src/main/java/it/polito/mad/g26/playingcourtreservation/activity/MainActivity.kt
@@ -2,18 +2,25 @@ package it.polito.mad.g26.playingcourtreservation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import it.polito.mad.g26.playingcourtreservation.R
+import it.polito.mad.g26.playingcourtreservation.util.isItemChecked
+import it.polito.mad.g26.playingcourtreservation.util.makeGone
+import it.polito.mad.g26.playingcourtreservation.util.setCheckedMenuItem
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNav: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav = findViewById(R.id.bottomNavigationView)
         val navController = (supportFragmentManager.findFragmentById(R.id.frame_layout)
                 as NavHostFragment).navController
 
@@ -21,21 +28,18 @@ class MainActivity : AppCompatActivity() {
         bottomNav.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.home -> navigateToFragment(
-                    bottomNav,
                     navController,
                     R.id.home,
                     R.id.searchCourtFragment
                 )
 
                 R.id.reservations -> navigateToFragment(
-                    bottomNav,
                     navController,
                     R.id.reservations,
                     R.id.reservationsFragment
                 )
 
                 R.id.profile -> navigateToFragment(
-                    bottomNav,
                     navController,
                     R.id.profile,
                     R.id.showProfileFragment
@@ -46,26 +50,21 @@ class MainActivity : AppCompatActivity() {
 
         // Handle Active Menu Item in the Navbar
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            when (destination.id) {
-                R.id.searchCourtFragment ->
-                    bottomNav.menu.findItem(R.id.home).isChecked = true
-
-                R.id.reservationsFragment ->
-                    bottomNav.menu.findItem(R.id.reservations).isChecked = true
-
-                R.id.showProfileFragment, R.id.editProfileFragment ->
-                    bottomNav.menu.findItem(R.id.profile).isChecked = true
+            Handler(Looper.getMainLooper()).post {
+                when (destination.id) {
+                    // Main Views: the bottom navigation must be visible
+                    R.id.searchCourtFragment -> bottomNav.setCheckedMenuItem(R.id.home)
+                    R.id.reservationsFragment -> bottomNav.setCheckedMenuItem(R.id.reservations)
+                    R.id.showProfileFragment -> bottomNav.setCheckedMenuItem(R.id.profile)
+                    // Sub Views: the bottom navigation should not be visible
+                    R.id.editProfileFragment -> bottomNav.makeGone()
+                }
             }
         }
     }
 
-    private fun navigateToFragment(
-        bottomNav: BottomNavigationView,
-        navController: NavController,
-        itemId: Int,
-        destinationId: Int
-    ) {
-        if (bottomNav.menu.findItem(itemId).isChecked) {
+    private fun navigateToFragment(navController: NavController, itemId: Int, destinationId: Int) {
+        if (bottomNav.isItemChecked(itemId)) {
             // If the current destination is already selected, do nothing
             return
         }
--- app/src/main/java/it/polito/mad/g26/playingcourtreservation/fragment/ShowProfileFragment.kt
+++ app/src/main/java/it/polito/mad/g26/playingcourtreservation/fragment/ShowProfileFragment.kt
@@ -1,7 +1,6 @@
 package it.polito.mad.g26.playingcourtreservation.fragment
 
 import android.content.Context
-import android.content.Intent
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.Menu
@@ -22,7 +21,6 @@ import it.polito.mad.g26.playingcourtreservation.activity.MainActivity
 import it.polito.mad.g26.playingcourtreservation.ui.CustomTextView
 import org.json.JSONObject
 
-
 class ShowProfileFragment : Fragment(R.layout.activity_show_profile) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -42,6 +40,7 @@ class ShowProfileFragment : Fragment(R.layout.activity_show_profile) {
                         findNavController().navigate(R.id.action_showProfileFragment_to_editProfileFragment)
                         true
                     }
+
                     else -> false
                 }
             }
@@ -57,37 +56,41 @@ class ShowProfileFragment : Fragment(R.layout.activity_show_profile) {
         val dangerIcon = view.findViewById<ImageView>(R.id.danger_icon)
         TooltipCompat.setTooltipText(dangerIcon, getString(R.string.danger_icon_tooltip))
 
-        loadProfikeAndImage()
+        loadProfileAndImage()
     }
 
-    fun loadProfikeAndImage(){
+    private fun loadProfileAndImage() {
 
         //PROFILE MANAGEMENT
         //Load if already exist, otherwise it will load the hardcoded data
         val sharedPref = this.requireActivity().getSharedPreferences("test", Context.MODE_PRIVATE)
-        if(sharedPref.contains("profile")){
-            val json= JSONObject(sharedPref.getString("profile","Default"))
-            val username =requireView().findViewById<TextView>(R.id.username)
-            username.text= json.getString("username")
-            val position=requireView().findViewById<TextView>(R.id.position)
-            position.text=json.getString("position")
-            val age=requireView().findViewById<CustomTextView>(R.id.age).findViewById<TextView>(R.id.value)
-            age.text=json.getString("age")
-            val gender=requireView().findViewById<CustomTextView>(R.id.gender).findViewById<TextView>(R.id.value)
-            gender.text=json.getString("gender")
-            val fullName=requireView().findViewById<CustomTextView>(R.id.fullname).findViewById<TextView>(R.id.value)
-            fullName.text=json.getString("fullName")
-            val location=requireView().findViewById<CustomTextView>(R.id.location).findViewById<TextView>(R.id.value)
-            location.text=json.getString("location")
+        if (sharedPref.contains("profile")) {
+            val json = sharedPref.getString("profile", "Default")?.let { JSONObject(it) }
+            val username = requireView().findViewById<TextView>(R.id.username)
+            username.text = json?.getString("username")
+            val position = requireView().findViewById<TextView>(R.id.position)
+            position.text = json?.getString("position")
+            val age = requireView().findViewById<CustomTextView>(R.id.age)
+                .findViewById<TextView>(R.id.value)
+            age.text = json?.getString("age")
+            val gender = requireView().findViewById<CustomTextView>(R.id.gender)
+                .findViewById<TextView>(R.id.value)
+            gender.text = json?.getString("gender")
+            val fullName = requireView().findViewById<CustomTextView>(R.id.fullname)
+                .findViewById<TextView>(R.id.value)
+            fullName.text = json?.getString("fullName")
+            val location = requireView().findViewById<CustomTextView>(R.id.location)
+                .findViewById<TextView>(R.id.value)
+            location.text = json?.getString("location")
         }
 
         //IMAGE MANAGEMENT
         //Load if exists, otherwise it will load the hardcoded image
         val file = requireContext().getFileStreamPath("imageBit")
-        if(file.exists()){
-            val fileInput= requireActivity().openFileInput("imageBit")
-            if(fileInput.available()>0){
-                val bitmap= BitmapFactory.decodeStream(fileInput)//already decompressed
+        if (file.exists()) {
+            val fileInput = requireActivity().openFileInput("imageBit")
+            if (fileInput.available() > 0) {
+                val bitmap = BitmapFactory.decodeStream(fileInput)//already decompressed
                 val avatarImage = requireView().findViewById<ImageView>(R.id.avatar)
                 avatarImage.setImageBitmap(bitmap)
             }
@@ -97,7 +100,7 @@ class ShowProfileFragment : Fragment(R.layout.activity_show_profile) {
 
     override fun onResume() {
         super.onResume()
-        loadProfikeAndImage()
+        loadProfileAndImage()
     }
 
 }
