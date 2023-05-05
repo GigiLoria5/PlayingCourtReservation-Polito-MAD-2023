28dbfabc01849b124bfdfa49b517e54dceb50409
Luigi Loria <41837640+GigiLoria5@users.noreply.github.com>
2023-05-05 10:06:41 +0200

    fix: avoid navigation overlap

--- app/src/main/java/it/polito/mad/g26/playingcourtreservation/activity/MainActivity.kt
+++ app/src/main/java/it/polito/mad/g26/playingcourtreservation/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package it.polito.mad.g26.playingcourtreservation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import it.polito.mad.g26.playingcourtreservation.R
@@ -12,21 +13,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navController = (
-                supportFragmentManager
-                    .findFragmentById(R.id.frame_layout) as NavHostFragment
-                ).navController
-
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navController = (supportFragmentManager.findFragmentById(R.id.frame_layout)
+                as NavHostFragment).navController
+
         // Handle Navigation between main fragments
         bottomNav.setOnItemSelectedListener {
             when (it.itemId) {
-                R.id.home -> navController.navigate(R.id.searchCourtFragment)
-                R.id.reservations -> navController.navigate(R.id.reservationsFragment)
-                R.id.profile -> navController.navigate(R.id.showProfileFragment)
+                R.id.home -> navigateToFragment(
+                    bottomNav,
+                    navController,
+                    R.id.home,
+                    R.id.searchCourtFragment
+                )
+
+                R.id.reservations -> navigateToFragment(
+                    bottomNav,
+                    navController,
+                    R.id.reservations,
+                    R.id.reservationsFragment
+                )
+
+                R.id.profile -> navigateToFragment(
+                    bottomNav,
+                    navController,
+                    R.id.profile,
+                    R.id.showProfileFragment
+                )
             }
             true
         }
+
         // Handle Active Menu Item in the Navbar
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
@@ -40,6 +57,19 @@ class MainActivity : AppCompatActivity() {
                     bottomNav.menu.findItem(R.id.profile).isChecked = true
             }
         }
+    }
+
+    private fun navigateToFragment(
+        bottomNav: BottomNavigationView,
+        navController: NavController,
+        itemId: Int,
+        destinationId: Int
+    ) {
+        if (bottomNav.menu.findItem(itemId).isChecked) {
+            // If the current destination is already selected, do nothing
+            return
+        }
+        navController.navigate(destinationId)
     }
 
 }
