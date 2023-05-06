e564d23dbbec73caf317fc899b1d5c59a0762423
RiccardoStud <s292520@studenti.polito.it>
2023-05-06 13:05:29 +0200

    feat: fragment reservation X+layout  updated

--- app/src/main/java/it/polito/mad/g26/playingcourtreservation/fragment/ReservationXFragment.kt
+++ app/src/main/java/it/polito/mad/g26/playingcourtreservation/fragment/ReservationXFragment.kt
@@ -1,47 +1,77 @@
 package it.polito.mad.g26.playingcourtreservation.fragment
 
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.*
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
 import it.polito.mad.g26.playingcourtreservation.R
 
 
+class ReservationXFragment : Fragment(R.layout.fragment_reservation_x) {
 
-class ReservationXFragment : Fragment(R.layout.fragment_reservation_x){
 
-    private lateinit var spinnerSer: Spinner
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         var serviceUsed=listOf("Bathroom", "Var Usage","First Aid Kit","Supporters","MiniBar", "CheatMode")
-            //getResources().getStringArray(R.array.services_array)
-
-        //Set spinner and adapter
-        spinnerSer = view.findViewById(R.id.spinner)
-        val adapterSer= ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, serviceUsed)
-        spinnerSer.adapter=adapterSer
-
-        spinnerSer.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
-                spinnerSer.setSelection(0)
-            }
-            override fun onNothingSelected(parent: AdapterView<*>) {
-                // Do nothing
-            }
-        }
+        //getResources().getStringArray(R.array.services_array)
 
 
         //Button for popup
-        val servicesButton=view.findViewById<Button>(R.id.buttonModify)
+        val servicesButton=view.findViewById<Button>(R.id.delete_reservation)
         servicesButton.setOnClickListener{
             //move to another fragment
         }
 
+
+        //recyclerview
+        val recyclerView = view.findViewById<RecyclerView>(R.id.service_list)
+        recyclerView.adapter=MyAdapterRecycle1(serviceUsed)
+        recyclerView.layoutManager= GridLayoutManager(context,3)
+
+
+
+
+
+
     }
+
 
 }
 
+//Class to contain the view of a single random item
+class MyViewHolder1 (v:View) : RecyclerView.ViewHolder(v){
 
+    private val cBox=v.findViewById<MaterialButton>(R.id.material_button)
+
+    fun bind(s:String){
+        cBox.text=s
+    }
+}
+
+
+class MyAdapterRecycle1( val l :List<String>) : RecyclerView.Adapter<MyViewHolder1>(){
+
+    //Inflater of the parent transform the xml of a row of the recyclerView into a view
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder1 {
+        val v= LayoutInflater.from(parent.context).inflate(R.layout.service_recycler,parent,false)
+        return MyViewHolder1(v)
+    }
+
+    //Need to know the max value of position
+    override fun getItemCount(): Int {
+        return l.size
+    }
+
+    //called after viewHolder are created, to put data into them
+    override fun onBindViewHolder(holder: MyViewHolder1, position: Int) {
+        holder.bind(l[position])
+    }
+}
