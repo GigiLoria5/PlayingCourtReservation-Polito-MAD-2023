84373118accc358865d84cdacc86af1cad7adee0
Luigi Loria <41837640+GigiLoria5@users.noreply.github.com>
2023-06-02 10:46:03 +0200

    feat: improve profile show, edit and skill layouts

--- app/src/main/java/it/polito/mad/g26/playingcourtreservation/adapter/UserSkillsAdapter.kt
+++ app/src/main/java/it/polito/mad/g26/playingcourtreservation/adapter/UserSkillsAdapter.kt
@@ -9,6 +9,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.button.MaterialButton
 import it.polito.mad.g26.playingcourtreservation.R
 import it.polito.mad.g26.playingcourtreservation.model.Skill
+import it.polito.mad.g26.playingcourtreservation.util.makeInvisible
+import it.polito.mad.g26.playingcourtreservation.util.makeVisible
 
 class UserSkillsAdapter(
     private var skills: List<Skill>,
@@ -20,9 +22,10 @@ class UserSkillsAdapter(
         parent: ViewGroup,
         viewType: Int
     ): UserSkillsViewHolder {
-        val v = LayoutInflater.from(parent.context)
-            .inflate(R.layout.show_profile_sport_item, parent, false)
-        return UserSkillsViewHolder(v)
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.profile_skill, parent, false
+        )
+        return UserSkillsViewHolder(view)
     }
 
     // Need to know the max value of position
@@ -45,25 +48,26 @@ class UserSkillsAdapter(
     }
 
     inner class UserSkillsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private val sportName = view.findViewById<TextView>(R.id.sport_title_show_profile)
-        private val rating = view.findViewById<RatingBar>(R.id.sport_rating_show_profile)
-        private val buttonDelete =
-            if (isEditable) view.findViewById<MaterialButton>(R.id.sport_delete_rating)
-            else null
+        private val sportName = view.findViewById<TextView>(R.id.sport_name)
+        private val rating = view.findViewById<RatingBar>(R.id.rating)
+        private val buttonDelete = view.findViewById<MaterialButton>(R.id.delete_rating)
 
         fun bind(skill: Skill) {
             sportName.text = skill.sportName
             rating.rating = skill.rating
-            if (isEditable) {
-                rating.setOnRatingBarChangeListener { _, newRating, _ ->
-                    val updatedSkill = skill.copy(rating = newRating)
-                    updateSkillInList(skill, updatedSkill)
-                }
-                buttonDelete!!.setOnClickListener {
-                    val updatedSkill = skill.copy(rating = 0f)
-                    updateSkillInList(skill, updatedSkill)
-                    rating.rating = 0f
-                }
+            if (!isEditable) {
+                buttonDelete.makeInvisible()
+                return
+            }
+            buttonDelete.makeVisible()
+            rating.setOnRatingBarChangeListener { _, newRating, _ ->
+                val updatedSkill = skill.copy(rating = newRating)
+                updateSkillInList(skill, updatedSkill)
+            }
+            buttonDelete.setOnClickListener {
+                val updatedSkill = skill.copy(rating = 0f)
+                updateSkillInList(skill, updatedSkill)
+                rating.rating = 0f
             }
         }
     }
