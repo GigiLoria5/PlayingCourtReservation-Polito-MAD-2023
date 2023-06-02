1cbf248cee0e6f1cceafb48e6e745ec0367c6bf0
antoniocolelli <colelli16@gmail.com>
2023-06-02 10:34:23 +0200

    feat: created repo function get available users (NOT YET TESTED)

--- app/src/main/java/it/polito/mad/g26/playingcourtreservation/viewmodel/InviteUsersViewModel.kt
+++ app/src/main/java/it/polito/mad/g26/playingcourtreservation/viewmodel/InviteUsersViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import it.polito.mad.g26.playingcourtreservation.model.Reservation
 import it.polito.mad.g26.playingcourtreservation.model.User
 import it.polito.mad.g26.playingcourtreservation.repository.ReservationRepository
 import it.polito.mad.g26.playingcourtreservation.repository.UserRepository
